76a37189547c00504d10e262e4e9cd16edc15aa4
Rakshita Vijay <rakshita.vijay1@gmail.com>
2024-11-18 16:04:55 +0530

    not working reducing dependencies using for loop, why only one step recorded???

--- DiSCo_Programming_Assignment_Answer.c
+++ DiSCo_Programming_Assignment_Answer.c
@@ -28,15 +28,18 @@ int main () {
 
   int arr_after_kahn_algo_to_print[str_split[0]][str_split[0]]; // square matrix of N rows and N columns
 
+  int impossible_step = -1;
+
   for (int outer_index = 0; outer_index < str_split[1]; outer_index++) {
     for (int inner_index = 0; inner_index < str_split[1]; inner_index++) {
-      arr_after_kahn_algo_to_print[outer_index][inner_index] = -1;
+      arr_after_kahn_algo_to_print[outer_index][inner_index] = impossible_step;
       // printf("%i, ", arr_after_kahn_algo_to_print[outer_index][inner_index]);
     }
   // printf(" * \n");
   }
 
   int in_degrees[str_split[0]];
+
   for(int num = 0; num < str_split[0]; num++) {
     for (int index_for_2nd_column = 0; index_for_2nd_column < str_split[1]; index_for_2nd_column++) {
       if ((num + 1) == m_dependencies_final_2nd_indices[index_for_2nd_column]) {
@@ -45,35 +48,54 @@ int main () {
     }
   }
 
-  printf(" ** \n");
+  printf("** \n");
 
   for (int index_for_in_degrees = 0; index_for_in_degrees < str_split[0]; index_for_in_degrees++) {
     printf("%i ", in_degrees[index_for_in_degrees]);
   }
 
-  printf(" ** \n");
+  printf("\n** \n \n");
 
-  // for (int outer_index = 0; outer_index < str_split[1]; outer_index++) {
-  //   for (int inner_index = 0; inner_index < str_split[1]; inner_index++) {
-  //     int place = check_for_dependencies(m_dependencies_final_1st_indices[outer_index], length_of_m_dependencies_final_2nd_indices, m_dependencies_final_2nd_indices);
-  //     if (place == -1) {
-  //       break;
-  //     } else {
-  //       arr_after_kahn_algo_to_print[outer_index][inner_index] = m_dependencies_final_1st_indices[place];
-  //       break;
-  //     }
-  //   }
-  // }
+  int outer_index = 0;
+  int inner_index = 0;
+  int pointer = 0;
 
-  printf("%li \n", (sizeof(arr_after_kahn_algo_to_print) / sizeof(arr_after_kahn_algo_to_print[0])));
+  for (int counter = 0; counter < str_split[0]; counter++) {
+    if (in_degrees[counter] == 0) {
+      arr_after_kahn_algo_to_print[outer_index][inner_index] = counter + 1;
+      inner_index++;
+      for (int checker = 0; checker < str_split[1]; checker++) {
+        if (m_dependencies_final_2nd_indices[checker] == counter + 1) {
+          in_degrees[m_dependencies_final_2nd_indices[checker] - 1] = in_degrees[m_dependencies_final_2nd_indices[checker] - 1] - 1;
+          pointer = 1;
+          printf("in_degrees for index %i, which means number %i: %i \n", m_dependencies_final_2nd_indices[checker] - 1, m_dependencies_final_2nd_indices[checker], in_degrees[m_dependencies_final_2nd_indices[checker] - 1]);
+        // } else if (pointer != 0) {
+        //   outer_index++;
+        }
+      }
+    }
+    outer_index++;
+  }
+
+  int length_of_final_run = 0;
+
+  for (int outer_counter = 0; ; outer_counter++) {
+    if (arr_after_kahn_algo_to_print[outer_counter][0] != impossible_step) {
+      length_of_final_run++;
+    } else {
+      break;
+    }
+  }
+
+  printf("%i \n", length_of_final_run);
 
   for (int outer_index = 0; outer_index < str_split[1]; outer_index++) {
     for (int inner_index = 0; inner_index < str_split[1]; inner_index++) {
-      if (arr_after_kahn_algo_to_print[outer_index][inner_index] > 0) {
+      if (arr_after_kahn_algo_to_print[outer_index][inner_index] != impossible_step) {
         printf("%i", arr_after_kahn_algo_to_print[outer_index][inner_index]);
       }
 
-      if (inner_index != (str_split[1] - 1) && (arr_after_kahn_algo_to_print[inner_index + 1] > 0)) {
+      if (inner_index != (str_split[1] - 1) && (arr_after_kahn_algo_to_print[outer_index][inner_index + 1] != impossible_step)) {
         printf(", ");
       } else {
         printf("\n");
--- teaching_v.c
+++
@@ -1,1 +0,0 @@
-Wiysvs n
